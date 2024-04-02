dbbf7f9a2b1c9e038fe30aa07ba1d4829a29319c
justKnick <126913469+justknick@users.noreply.github.com>
2024-04-02 14:39:16 -0400

    player - added script (movement)

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+const MAX_SPEED = 200
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +10,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+
+func get_movement_vector():
+	var movement_vector = Vector2.ZERO
+	
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	return Vector2(x_movement, y_movement)
