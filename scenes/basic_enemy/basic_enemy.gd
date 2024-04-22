ed306d1110b98c2d5c00864b7612075c403b349b
justKnick <126913469+justknick@users.noreply.github.com>
2024-04-22 19:53:44 -0400

    updated basic_enemy scene (updated func process to allow enemy to move)

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -10,10 +10,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var direction = get_direction_to_player()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
 
 
-#
 func get_direction_to_player():
 	var player_node = get_tree().get_first_node_in_group("player") as Node2D
 	if player_node != null:
