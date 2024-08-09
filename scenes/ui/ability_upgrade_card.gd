8d7487f43fd75d7001c3b3d71ef5aeea02aac383
justKnick <126913469+justknick@users.noreply.github.com>
2024-08-09 18:06:25 -0400

    Section 2.23 - Enabling Upgrade Selection // updated upgrade_screen script (added upgrade_selected signal, queue free and resume game), ability_upgrade_card script (added selected signal and logic for left_click input), upgrade_manager script (connected on_level_up with apply_upgrade), and godot project (added left_click input map)

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -1,9 +1,22 @@
 extends PanelContainer
 
+signal selected
+
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
+
+
+# if a player clicks on a card, then that card will be chosen 
+# then close screen and resume game 
+func _ready():
+	gui_input.connect(on_gui_input)
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
 	name_label.text = upgrade.name
 	description_label.text = upgrade.description
+
+
+func on_gui_input(event: InputEvent):
+	if event.is_action_pressed("left_click"):
+		selected.emit()
