a93aebbcf6f2d89f0234e6b4b95e6bfb26459806
justKnick <126913469+justknick@users.noreply.github.com>
2024-08-10 12:59:18 -0400

    Section 3.30 - Increase Difficulty Over Time // updated enemy_manager scene (implement enemy spawn time reduction per difficulty increase), arena_time_manager scene (implemented difficulty increase, updated wait time to 300s), and main scene (linked arena_time_manager to enemy_manager node)

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,12 +1,27 @@
 extends Node
 
+signal arena_difficulty_increased(arena_difficulty: int)
+
+const DIFFICULTY_INTERVAL = 5
+
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
  
+var arena_difficulty = 0
+
 
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
+
+
+func _process(delta):
+	# create increment of arena difficulty 
+	# take current area difficulty (add one to it) multiplied by DIFFICULTY INTERVAL
+	var next_time_target = timer.wait_time - ((arena_difficulty + 1 ) * DIFFICULTY_INTERVAL)
+	if timer.time_left <= next_time_target:
+		arena_difficulty += 1
+		arena_difficulty_increased.emit(arena_difficulty)
 
 
 func  get_time_elapse():
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,21 +3,31 @@ extends Node
 const SPAWN_RADIUS = 350
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+
+@onready var timer = $Timer 
+
+var base_spawn_time = 0
 
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
-	
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 
 
+# func for enemy spawn rate 
 func on_timer_timeout():
+	# restart timer, update wait timer on next timeout signal
+	timer.start()
+	
 	# want to spawn enemy
 	# don't want to spawn on player
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	# want to make sure player exists - isn't KO'd 
 	if player == null:
 		return 
-		
+	
 	# need to declare spawn distance from 0 to 2 tau 
 	# RIGHT means the RIGHT direction, aka 0 degrees or 0 radians 
 	# rand range will rotate location randomly between 0 and 2 pi (or 360 degrees) 
@@ -32,3 +42,14 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position 
+
+
+func on_arena_difficulty_increased(arena_difficulty: int):
+	# update wait_time here, wait for next timeout to use new wait_time
+	var time_reduce = (0.1 / 12) * arena_difficulty
+	
+	# give time_reduce a limit - do not go smaller than 0.7
+	time_reduce = min(time_reduce, .7)
+	
+	print(time_reduce)
+	timer.wait_time = base_spawn_time - time_reduce
