acc36be500451443e7615b20f443f4802e1ff4be
justKnick <126913469+justknick@users.noreply.github.com>
2024-08-09 20:43:45 -0400

    Section 2.27 - Player Health Bar // updated health_component script (added get_health_percent function and health_change signal), player scene (stylized healthbar, added update_health_display func, health component and health bar)

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -2,6 +2,7 @@ extends Node
 class_name HealthComponent
 
 signal fainted
+signal health_change
 
 @export var max_health: float = 10 
 var current_health 
@@ -13,7 +14,14 @@ func _ready():
 
 func damage(damage_amount:float):
 	current_health = max(current_health - damage_amount, 0)
+	health_change.emit()
 	Callable(check_fainted).call_deferred()
+
+
+func get_health_percent():
+	if current_health <= 0:
+		return 0
+	return min(current_health / max_health, 1) 
 
 
 func check_fainted():
--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -4,6 +4,8 @@ const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25 
 
 @onready var damage_interval_timer = $DamageIntervalTimer
+@onready var health_component = $HealthComponent
+@onready var health_bar = $HealthBar
 
 var number_collision_bodies = 0
 
@@ -12,6 +14,8 @@ func _ready():
 	$CollisionArea2D.body_entered.connect(on_body_entered)
 	$CollisionArea2D.body_exited.connect(on_body_exited)
 	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
+	health_component.health_change.connect(on_health_changed)
+	update_health_display()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -32,25 +36,33 @@ func get_movement_vector():
 
 
 func check_deal_damage():
-	# deal damage while enemy(ies) enter player collision
+	# deal damage while enemy(ies) are in player collision
 	# check for (1)no bodies in player OR (2) damage_interbal_timer is running
 	if number_collision_bodies == 0 || !damage_interval_timer.is_stopped():
 		return
-	$HealthComponent.damage(1)
+	health_component.damage(1)
 	$DamageIntervalTimer.start()
-	print($HealthComponent.current_health)
+	print(health_component.current_health)
+
+
+func update_health_display():
+	health_bar.value = health_component.get_health_percent()
 
 
 func on_body_entered(other_body: Node2D):
-	# player will take damage WHILE enemy is colliding with player
+	# count bodies entering player collision
 	number_collision_bodies += 1
 	on_damage_interval_timer_timeout()
 
 
 func on_body_exited(other_body: Node2D):
-	# 
+	# count bodies leaving player collision
 	number_collision_bodies -= 1
 
 
 func on_damage_interval_timer_timeout():
 	check_deal_damage()
+
+
+func on_health_changed():
+	update_health_display()
