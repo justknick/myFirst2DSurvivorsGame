f04edd7ac12e96fedaf999455ce1c609d1ed55c5
justknick <126913469+justknick@users.noreply.github.com>
2024-08-24 19:44:50 -0400

    Section 3.40 - Using Weighted Table for Enemy Spawn // created weighted_table script // updated enemy_manager scene (change logic to add enemies to a dictionary and using weighted table to spawn enemies)

--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,14 +3,18 @@ extends Node
 const SPAWN_RADIUS = 350
 
 @export var basic_enemy_scene: PackedScene
+@export var wizard_enemy_scene: PackedScene
 @export var arena_time_manager: Node
 
 @onready var timer = $Timer 
 
 var base_spawn_time = 0
+var enemy_table = WeightedTable.new()
 
 
 func _ready():
+	# add basic enemy to the enemy table 
+	enemy_table.add_item(basic_enemy_scene, 10)
 	base_spawn_time = timer.wait_time
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
@@ -55,8 +59,10 @@ func on_timer_timeout():
 	if player == null:
 		return 
 	
+	# pick an enemy from the table from ready function
+	var enemy_scene = enemy_table.pick_item()
 	# declare enemy to instantiate - this creates node, but doesn't put in scene tree
-	var enemy = basic_enemy_scene.instantiate() as Node2D
+	var enemy = enemy_scene.instantiate() as Node2D
 	
 	# now we'll place enemy node into scene tree, under entities layer
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
@@ -73,3 +79,7 @@ func on_arena_difficulty_increased(arena_difficulty: int):
 	
 	print(time_reduce)
 	timer.wait_time = base_spawn_time - time_reduce
+	
+	if arena_difficulty == 6: 
+		enemy_table.add_item(wizard_enemy_scene, 20)
+
