f901dd88789e77fee9a4a9e2b976617d3e6c15f4
justknick <126913469+justknick@users.noreply.github.com>
2024-08-24 20:42:18 -0400

    Section 3.41 - Animating the Wizard // updated wizard_enemy scene (create animation player, update script to account for stop movement), velocity script (created decelerate funcion for stop movement)

--- scenes/component/velocity_component.gd
+++ scenes/component/velocity_component.gd
@@ -33,6 +33,10 @@ func accelerate_in_direction(direction: Vector2):
 	velocity = velocity.lerp(desired_velocity, 1 - exp(-acceleration * get_process_delta_time()))
 
 
+func decelerate():
+	accelerate_in_direction(Vector2.ZERO)
+
+
 # func used to move the character by calling it 
 func move(character_body: CharacterBody2D):
 	character_body.velocity = velocity
--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,8 +3,19 @@ extends CharacterBody2D
 @onready var visuals = $Visuals
 @onready var velocity_component = $VelocityComponent
 
+var is_moving = false
+
 
 func _process(delta):
-	velocity_component.accelerate_to_player()
+	# wizard movement will have a slight pause
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else: 
+		velocity_component.decelerate()
+	
 	velocity_component.move(self)
 	velocity_component.get_facing_direction(visuals)
+
+
+func set_is_moving(moving: bool):
+	is_moving = moving
