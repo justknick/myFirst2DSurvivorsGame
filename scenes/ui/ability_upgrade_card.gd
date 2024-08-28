33f15effec881c3e3118a9aae696c30575822c74
justknick <126913469+justknick@users.noreply.github.com>
2024-08-28 18:56:52 -0400

    Section 3.49 - Animating the Upgrade Card // updated ability_upgrade_card (implemented card mouse_in, hover, selected, discard animations), upgrade_screen (implemented asycn to delay card draw)

--- scenes/ui/ability_upgrade_card.gd
+++ scenes/ui/ability_upgrade_card.gd
@@ -5,11 +5,24 @@ signal selected
 @onready var name_label: Label = %NameLabel
 @onready var description_label: Label = %DescriptionLabel
 
+var disabled = false
+
 
 # if a player clicks on a card, then that card will be chosen 
 # then close screen and resume game 
 func _ready():
 	gui_input.connect(on_gui_input)
+	mouse_entered.connect(on_mouse_entered)
+
+
+func play_in(delay: float = 0):
+	# start card as transparent
+	modulate = Color.TRANSPARENT
+	# add delay to animate cards one at a time
+	# we are going to 'await' running this func until timeout signal  
+	await get_tree().create_timer(delay).timeout
+	
+	$AnimationPlayer.play("in")
 
 
 func set_ability_upgrade(upgrade: AbilityUpgrade):
@@ -17,6 +30,33 @@ func set_ability_upgrade(upgrade: AbilityUpgrade):
 	description_label.text = upgrade.description
 
 
+func play_discard():
+	$AnimationPlayer.play("discard")
+
+
+func select_card():
+	disabled = true
+	$AnimationPlayer.play("selected")
+	
+	for other_card in get_tree().get_nodes_in_group("upgrade_card"):
+		if other_card == self:
+			continue
+		other_card.play_discard()
+	
+	await $AnimationPlayer.animation_finished
+	selected.emit()
+
+
 func on_gui_input(event: InputEvent):
+	if disabled:
+		return
 	if event.is_action_pressed("left_click"):
-		selected.emit()
+		select_card()
+
+
+func on_mouse_entered():
+	if disabled:
+		return
+	$HoverAnimationPlayer.play("hover")
+	
+
