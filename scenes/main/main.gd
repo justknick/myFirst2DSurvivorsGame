aa71a5c665401e5e4db43a4c72efae6c30016964
justknick <126913469+justknick@users.noreply.github.com>
2024-09-01 11:35:38 -0400

    Section 5.66 - Creating a Pause Menu // created pause manu scene // updated options_menu (updated layer), main scene (implemented pause menu), godot project (added input for pause)

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,9 +2,20 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
+var pause_menu_screen = preload("res://scenes/ui/pause_menu.tscn")
+
 
 func _ready():
 	%Player.health_component.fainted.connect(on_player_fainted)
+
+
+# listen for pause action
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		add_child(pause_menu_screen.instantiate())
+		# whatever current input event being sent through tree is, 
+		#  it'll be marked as 'handled' and stop its propagation
+		get_tree().root.set_input_as_handled()
 
 
 func on_player_fainted():
---
+++ scenes/ui/pause_menu.gd
@@ -0,0 +1,67 @@
+extends CanvasLayer
+
+@onready var panel_container = %PanelContainer
+
+var is_closing
+var options_menu_scene = preload("res://scenes/ui/options_menu.tscn")
+
+
+func _ready():
+	get_tree().paused = true
+	panel_container.pivot_offset = panel_container.size / 2
+	
+	$%ResumeButton.pressed.connect(on_resume_pressed)
+	$%OptionsButton.pressed.connect(on_options_pressed)
+	$%QuitToTitleButton.pressed.connect(on_quit_to_title_pressed)
+	$AnimationPlayer.play("default")
+	
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+
+
+# listen for pause action
+func _unhandled_input(event):
+	if event.is_action_pressed("pause"):
+		close()
+		# whatever current input event being sent through tree is, 
+		#  it'll be marked as 'handled' and stop its propagation
+		get_tree().root.set_input_as_handled()
+
+
+func close():
+	if is_closing:
+		return
+	is_closing = true
+	
+	$AnimationPlayer.play_backwards("default")
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ONE, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, .3)\
+	.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_BACK)
+	
+	await tween.finished
+	# release the pause state
+	get_tree().paused = false
+	queue_free()
+
+
+func on_resume_pressed():
+	close()
+
+
+func on_options_pressed():
+	var options_instance = options_menu_scene.instantiate()
+	add_child(options_instance)
+	options_instance.back_pressed.connect(on_options_closed.bind(options_instance))
+
+
+func on_options_closed(options_instance: Node):
+	options_instance.queue_free()
+
+
+func on_quit_to_title_pressed():
+	# release the pause state
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
